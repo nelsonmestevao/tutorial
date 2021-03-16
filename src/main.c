58ce6e5a4e5f133824f6ac00123274f86e5d7153
Nelson Estevão <email@nelson.com>
2021-03-16 03:48:39 +0000

    Adiciona função hello

--- src/main.c
+++ src/main.c
@@ -2,21 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-/**
- * Importante
- * @param argc quantos argumentos
- * */
+void hello(char* name) {
+    printf("Hello, %s!", name);
+}
+
 int main(int argc, char **argv) {
-    if (argc < 1) {
-        printf("Unexpected number of arguments\n");
-        return -1;
-    }
-
-    printf("You entered in reverse order:\n\n");
-
-    while (argc--) {
-        printf("* %s\n", argv[argc]);
-    }
+    hello("world");
 
     return 0;
 }
